a85142ae67d8ad95f603b2892faefa56fb27d9eb
erginbostanci <erginbostanci@gmail.com>
2019-03-16 14:08:01 -0500

    the last stand-2

--- main/js/app.js
+++ main/js/app.js
@@ -1,14 +1,14 @@
 // Importing data from data.js
-var tableData = data;
+var data_table = data;
 
-// YOUR CODE HERE!
+
 // Using d3.select to find the tbody DOM element in the html
 var tbody = d3.select("tbody"); 
 
 // Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
 
 // For every data in tableData a forEach() method that appends both table row and data with the value
-tableData.forEach((entry) => {
+data_table.forEach((entry) => {
     var row = tbody.append("tr");
     Object.entries(entry).forEach(([key, value]) => {
         var cell = tbody.append("td");
@@ -18,17 +18,17 @@ tableData.forEach((entry) => {
 
 // Solution adapted from StackOverflow questions by GrumpyCrouton & Matt Brown on filtering table data
 // Defining variables for now filtered data as sightings and the filter button
-var sightings = data;
-var filterButton = d3.select("#filter-btn");
-var clearButton = d3.select("#clear-btn");
+var sightings_data = data;
+var button_filter = d3.select("#filter-btn");
+var button_clear = d3.select("#clear-btn");
 
 // Creates the clear function on click
-clearButton.on("click", function() {
+button_clear.on("click", function() {
     // Prevents reloading the page, except kept here for illustrative purposes - functionally want the page to reload
     // d3.event.preventDefault();
     
     // Repopulates the data
-    tableData.forEach((entry) => {
+    data_table.forEach((entry) => {
         var row = tbody.append("tr");
         Object.entries(entry).forEach(([key, value]) => {
             var cell = tbody.append("td");
@@ -38,72 +38,70 @@ clearButton.on("click", function() {
 });
 
 // Creates the filter function on click
-filterButton.on("click", function() {
+button_filter.on("click", function() {
     // Prevents reloading the page
     d3.event.preventDefault();
     // Removes all data in the selection
     d3.selectAll("td").remove()
 
     // Using d3.select to find the datetime DOM element in the html
-    var inputElementdatetime = d3.select("#datetime");
-    var inputElementcity = d3.select("#city");
-    var inputElementstate = d3.select("#state");
-    var inputElementcountry = d3.select("#country");
-    var inputElementshape = d3.select("#shape");
+    var input_datetime = d3.select("#datetime");
+    var input_city = d3.select("#city");
+    var input_state = d3.select("#state");
+    var input_country = d3.select("#country");
+    var input_shape = d3.select("#shape");
 
     // The user's search query
-    var inputValuedatetime = inputElementdatetime.property("value");
-    var inputValuecity = inputElementcity.property("value");
-    var inputValuestate = inputElementstate.property("value");
-    var inputValuecountry = inputElementcountry.property("value");
-    var inputValueshape = inputElementshape.property("value");
+    var input_Value_datetime = input_datetime.property("value");
+    var input_Value_city = input_city.property("value");
+    var input_Value_state = input_state.property("value");
+    var input_Value_country = input_country.property("value");
+    var input_Value_shape = input_shape.property("value");
 
     // Filters data to entries matching the search query inputValue
-    var filteredDatadatetime = sightings.filter(sighting => sighting.datetime === inputValuedatetime);
-    var filteredDatacity = sightings.filter(sighting => sighting.city === inputValuecity);
-    var filteredDatastate = sightings.filter(sighting => sighting.state === inputValuestate);
-    var filteredDatacountry = sightings.filter(sighting => sighting.country === inputValuecountry);
-    var filteredDatashape = sightings.filter(sighting => sighting.shape === inputValueshape);
+    var filtered_Datadatetime = sightings_data.filter(sighting => sighting.datetime === input_Value_datetime);
+    var filtered_Datacity = sightings_data.filter(sighting => sighting.city === input_Value_city);
+    var filtered_Datastate = sightings_data.filter(sighting => sighting.state === input_Value_state);
+    var filtered_Datacountry = sightings_data.filter(sighting => sighting.country === input_Value_country);
+    var filtered_Datashape = sightings_data.filter(sighting => sighting.shape === input_Value_shape);
 
     // Using d3.select to find the tbody DOM element in the html, echoes the initial/default tbody variable
     var tbody = d3.select("tbody");
 
     // For every data in tableData, a forEach() method that appends both table row and data with the now filtered value(s)
-    filteredDatadatetime.forEach((filteredSightings) => {
+    filtered_Datadatetime.forEach((filtered_Sightings_data) => {
+        var row = tbody.append("tr");
+        Object.entries(filtered_Sightings_data).forEach(([key, value]) =>{
+            var cell = tbody.append("td");
+            cell.text(value);
+        });
+    });
+    filtered_Datacity.forEach((filtered_Sightings_data) => {
+        var row = tbody.append("tr");
+        Object.entries(filtered_Sightings_data).forEach(([key, value]) =>{
+            var cell = tbody.append("td");
+            cell.text(value);
+        });
+    });
+    filtered_Datastate.forEach((filtered_Sightings_data) => {
+        var row = tbody.append("tr");
+        Object.entries(filtered_Sightings_data).forEach(([key, value]) =>{
+            var cell = tbody.append("td");
+            cell.text(value);
+        });
+    });
+    filtered_Datacountry.forEach((filtered_Sightings_data) => {
         var row = tbody.append("tr");
         Object.entries(filteredSightings).forEach(([key, value]) =>{
             var cell = tbody.append("td");
             cell.text(value);
         });
     });
-    filteredDatacity.forEach((filteredSightings) => {
+    filtered_Datashape.forEach((filtered_Sightings_data) => {
         var row = tbody.append("tr");
-        Object.entries(filteredSightings).forEach(([key, value]) =>{
-            var cell = tbody.append("td");
-            cell.text(value);
-        });
-    });
-    filteredDatastate.forEach((filteredSightings) => {
-        var row = tbody.append("tr");
-        Object.entries(filteredSightings).forEach(([key, value]) =>{
-            var cell = tbody.append("td");
-            cell.text(value);
-        });
-    });
-    filteredDatacountry.forEach((filteredSightings) => {
-        var row = tbody.append("tr");
-        Object.entries(filteredSightings).forEach(([key, value]) =>{
-            var cell = tbody.append("td");
-            cell.text(value);
-        });
-    });
-    filteredDatashape.forEach((filteredSightings) => {
-        var row = tbody.append("tr");
-        Object.entries(filteredSightings).forEach(([key, value]) =>{
+        Object.entries(filtered_Sightings_data).forEach(([key, value]) =>{
             var cell = tbody.append("td");
             cell.text(value);
         });
     });
 });
-
-// Has a function for filtering, uses ".this"
